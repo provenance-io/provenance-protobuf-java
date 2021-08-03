98c2692b68f7550b6e2b62a8dafea742b1e0945a
Stephen Cirner <scirner@figure.com>
2021-08-03 16:48:29 -0400

    Bump to latest versions defined in provenance release 1.5.0.

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -1,10 +1,10 @@
 object Versions {
-    const val cosmos = "0.42.4"
-    const val wasmd = "v0.16.0"
-    const val pbProto = "1.3.0"
+    const val cosmos = "0.42.6"
+    const val wasmd = "v0.17.0"
+    const val pbProto = "1.5.0"
     const val protobuf = "3.6.1"
     const val protoUtil = "0.1.5"
-    const val grpc = "1.32.2"
+    const val grpc = "1.39.0"
     const val kotlin = "1.4.30"
     const val springboot = "2.3.5.RELEASE"
 
