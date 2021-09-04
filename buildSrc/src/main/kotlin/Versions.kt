eba97f21754cc0246a1ab5ea36e6ab62a0973c33
Stephen Cirner <scirner@figure.com>
2021-09-04 12:17:56 -0400

    Bump to provenance 1.7.0.

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -1,12 +1,11 @@
 object Versions {
-    const val cosmos = "0.42.6"
-    const val wasmd = "v0.17.0"
-    const val pbProto = "1.5.0"
+    const val cosmos = "0.44.0"
+    const val wasmd = "v0.19.0"
+    const val pbProto = "1.7.0"
     const val protobuf = "3.6.1"
     const val protoUtil = "0.1.5"
     const val grpc = "1.39.0"
     const val kotlin = "1.4.30"
-    const val springboot = "2.3.5.RELEASE"
 
     // plugins
     const val ktlintPlugin = "9.4.1"
