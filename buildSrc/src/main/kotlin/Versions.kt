75e3246c836131d711d724934d7ea5731f3bbe87
Mike Woods <mwoods@figure.com>
2022-01-11 00:17:39 -0500

    Update protobuf version to 3.19.2 + plugin patch version

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -2,12 +2,11 @@ object Versions {
     const val cosmos = "0.44.0"
     const val wasmd = "v0.19.0"
     const val pbProto = "1.7.0"
-    const val protobuf = "3.6.1"
-    const val protoUtil = "0.1.5"
+    const val protobuf = "3.19.2"
     const val grpc = "1.39.0"
     const val kotlin = "1.4.30"
 
     // plugins
-    const val ktlintPlugin = "9.4.1"
-    const val protobufPlugin = "0.8.15"
+    const val ktlintPlugin = "10.2.1"
+    const val protobufPlugin = "0.8.18"
 }
